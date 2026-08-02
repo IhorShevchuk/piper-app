7db2ae582d388dfea99f228fc3a111cc06dc978e
Ihor <ihorshev@gmail.com>
2026-08-01 19:05:36 -0700

    Updated Piper-ObjC that uses original piper1-gpl

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ import PackageDescription
 let package = Package(
     name: "Piper",
     dependencies: [
-        .package(url: "https://github.com/IhorShevchuk/piper-objc", from: "0.2.23")
+        .package(url: "https://github.com/IhorShevchuk/piper-objc", from: "0.2.24")
     ],
     targets: []  // Tuist uses this only to resolve deps; targets are in Project.swift via .external()
 )
