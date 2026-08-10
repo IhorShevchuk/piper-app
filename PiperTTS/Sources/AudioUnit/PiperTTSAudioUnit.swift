3e7595687b444719540812ef921d42ea5d9d385d
Ihor <ihorshev@gmail.com>
2026-08-09 17:48:12 -0700

    Scoped setting memoryThresholdBytes for iOS only for now

--- PiperTTS/Sources/AudioUnit/PiperTTSAudioUnit.swift
+++ PiperTTS/Sources/AudioUnit/PiperTTSAudioUnit.swift
@@ -207,11 +207,13 @@ public class PiperTTSAudioUnit: AVSpeechSynthesisProviderAudioUnit {
                       andConfigPath: paths.json.path(percentEncoded: false))
         
         Log.debug("Piper Created")
+#if os(iOS)
         let availableMemory = Int64(Double(os_proc_available_memory()) * 0.9)
         if availableMemory > 0 {
             Log.debug("Setting memoryThresholdBytes:\(ByteCountFormatter.string(fromByteCount: availableMemory, countStyle: .binary))")
             piper?.memoryThresholdBytes = UInt64(availableMemory)
         }
+#endif
         piper?.delegate = self
         self.model = model
     }
