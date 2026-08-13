8c0610b958d59e0725fced2e4553ec1d5c8d6e6a
Ihor <ihorshev@gmail.com>
2026-08-12 22:09:13 -0700

    Updated Piper-ObjC

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ import PackageDescription
 let package = Package(
     name: "Piper",
     dependencies: [
-        .package(url: "https://github.com/IhorShevchuk/piper-objc", from: "0.2.25")
+        .package(url: "https://github.com/IhorShevchuk/piper-objc", from: "0.2.26")
     ],
     targets: []  // Tuist uses this only to resolve deps; targets are in Project.swift via .external()
 )
